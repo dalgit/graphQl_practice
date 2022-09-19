6d1edcccca76234932e9391b68fb16b0dd39ca9f
junki <junki646@gmail.com>
2022-09-20 00:35:54 +0900

    [P1] Finally : 학습 완료

---
+++ odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Layout, QueryResult } from "../components";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Tracks;
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
