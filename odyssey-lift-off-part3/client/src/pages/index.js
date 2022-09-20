f52d1b6ea54b772e50aa2a7a92619bdc0f471d0b
junki <junki646@gmail.com>
2022-09-20 20:31:26 +0900

    [P3] Finally : 학습 완료

--- odyssey-lift-off-part3/client/src/pages/index.js
+++ odyssey-lift-off-part3/client/src/pages/index.js
@@ -1,12 +1,13 @@
-import React, { Fragment } from 'react';
-import { Router } from '@reach/router';
-/** importing our pages */
-import Tracks from './tracks';
+import React, { Fragment } from "react";
+import { Router } from "@reach/router";
+import Track from "./track";
+import Tracks from "./tracks";
 
 export default function Pages() {
   return (
     <Router primary={false} component={Fragment}>
       <Tracks path="/" />
+      <Track path="/track/:trackId" />
     </Router>
   );
 }
